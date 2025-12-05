edbf858e2c5feda72f55f3335094712f6823cd08
Sannidhi Hegde <sannidhi664@gmail.com>
2025-12-05 14:59:42 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -30,55 +30,7 @@ app.add_middleware(
 def on_startup():
     create_db_and_tables()
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
 @app.get("/")
 def home():
     return {"message": "Mental Health Tracker API is running"}
 
-
-
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from backend.auth import router
-# from backend.database import create_db_and_tables
-# from backend.diary import router as diary_router
-# from backend.psychometrics import psychometric_router
-# from backend.chatbot import chatbot_router
-# from backend.recommendations import recommendation_router
-# app = FastAPI()
-
-# app.include_router(router) 
-# app.include_router(diary_router)
-# app.include_router(psychometric_router)
-# app.include_router(chatbot_router)
-# app.include_router(recommendation_router)
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# @app.on_event("startup")
-# def on_startup():
-#     create_db_and_tables()
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# @app.get("/")
-# def home():
-#     return {"message": "Mental Health Tracker API is running"}
