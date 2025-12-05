d5221495110f082ec9d89edfaffd0721ed22cce6
Sannidhi Hegde <sannidhi664@gmail.com>
2025-12-05 14:58:09 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, create_engine, Session
 
 # Replace with your actual DB credentials
-DB_URL = "mysql+mysqlconnector://root:Iamtopper1!@localhost/mental_health_db"
+DB_URL = "mysql+mysqlconnector://root:password@localhost/mental_health_db"
 engine = create_engine(DB_URL, echo=True)
 
 def create_db_and_tables():
